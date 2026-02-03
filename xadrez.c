fa178da18c80ee435628a54479bf65288d560640
MatheusK27 <matheuskleinm@hotmail.com>
2026-02-02 21:47:21 -0300

    Update xadrez.c

--- xadrez.c
+++ xadrez.c
@@ -1,38 +1,47 @@
-#include<stdio.h>
+    #include<stdio.h>
+ 
+    
 
-int main(){
-int t,b,r,c;
-int movimento =1;
-
-//t=torre, b=bispo, r=rainha.
-
-for( t=0; t<5;  t++ ){
-    printf("Torre a direita\n");
-
-}
-
-
-for(b=0; b<5; b++){
-    printf("Bispo cima, a direita\n");
-        
-}
-
-
-for(r=0; r<8; r++){
-    printf("Rainha a esquerda\n");
-
-}
-
-while (movimento --)
-{
-    for(c=0 ; c<=2; c++){
-        
-        printf("baixo\n");
+    void moverTorre(int numero){ // movimento torre
+        if(numero>0){
+            printf("Torre a direita\n");
+            moverTorre(numero -1);
+        }
     }
-    printf("esquerda\n");
-}
+
+    void moverBispo(int numero){ // movimento bispo
+        if (numero>0){
+            printf("Bispo cima a direita\n");
+            moverBispo(numero -1);
+        }
+    }
+    
+    void moverrainha(int numero){ //movimento rainha
+        if(numero>0){
+            printf("Rainha a esquerda\n");
+            moverrainha(numero -1);
+        }
+    }
 
 
+    int main(){
+    moverTorre(5);
+    moverBispo(5);
+    moverrainha(8);
+
+    for( int c=0 ,d=0;  c<=2 && d<=1;c++, d++){ // movimento cavalo
+        printf("cavalo cima a direita\n");
+    
+    }
+    
+    for(int v =0;v<=1; v++){ // movimento bispo
+        printf("Bispo cima\n");
+        for(int h=0; h<=0;h++){
+            printf("Bispo direita\n");
+        }
+    }
 
 
-}
+    return 0;
+ }
+
